a80836273a96415615b05f743215483bc8a38245
Michael_0619 <any_leon@163.com>
2023-06-30 16:59:20 +0800

    [play]add play fun

--- app/src/main/java/com/example/beatbox/manager/BeatBox.kt
+++ app/src/main/java/com/example/beatbox/manager/BeatBox.kt
@@ -19,6 +19,19 @@ class BeatBox(private val assets: AssetManager) {
         sounds = loadSounds()
     }
 
+    /*
+    * 播放音频
+    * 检查通过后， 就可以调用SoundPool.play(Int, Float, Float, Int, Int, Float)函数播放音频了。
+    * 这些参数依次是： 音频ID、 左音量、 右音量、 优先级（无效） 、 是否循环和播放速率。 我们需要最大音量
+    * 和常速播放， 所以传入值1.0。 是否循环参数传入0， 代表不循环。 （如果想无限循环， 可以传入-1。 相信
+    * 这会超级讨人厌。 ）
+    * */
+    fun play(sound: Sound) {
+        sound.soundId?.let {
+            soundPool.play(it, 1.0f, 1.0f, 1, 0, 1.0f)
+        }
+    }
+
     private fun loadSounds(): List<Sound> {
         var soundNames: Array<String>
         try {

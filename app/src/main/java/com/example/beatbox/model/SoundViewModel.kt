e6d3b33395cd13b598bcc11417a8e0459d7c699d
Michael_0619 <any_leon@163.com>
2023-06-30 16:26:45 +0800

    [BaseObservable]Make the view model visible

--- app/src/main/java/com/example/beatbox/model/SoundViewModel.kt
+++ app/src/main/java/com/example/beatbox/model/SoundViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.beatbox.model
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
 import com.example.beatbox.manager.Sound
 
-class SoundViewModel {
+class SoundViewModel : BaseObservable() {
     var sound: Sound? = null
         set(sound) {
             field = sound
         }
+
+    @get:Bindable
     val title: String?
         get() = sound?.name
 }
